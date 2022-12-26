1f5be4afa57ca3fc865345c1d4200523ec1e6cda
J.M Winbush <jawonwinbush@gmail.com>
2022-12-26 00:36:19 -0500

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -14,21 +14,21 @@ const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      video: "https://cdn-cf-east.streamable.com/video/mp4/4gooer.mp4?Expires=1671959340&Signature=Rfq3nqaWPXx15E98U4h10k6~vhxrAMcZ4CB7yR4RFdeCyDWNpHMby2G8xcGydRnm7NFgcRlLtCrscYsE0usX37S24nC33sT8O1132By~OouL9VOQgj~vZRPzuzwnywgvCAmhlcRPXAH4kwtu81V0-Awp6HodjiyF1LF7I9ar4zaP2sweiO3ty4wd48ZZAF2dc8E88ITgn7mexATo7Lqcpz0ED5kP0Z9yV6VcbJb0C0bWphvl0m0jc4FDmuTU5KlNe60eDfLte6rTvn-vLrItXaCNB-X0Wgr5oTciCPWUIBUC1uM-5ZEj3xrE--6gKdY8NiklmpZfwnf0vHGbOkAyaA__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
+      video: "https://cdn-cf-east.streamable.com/video/mp4/4gooer.mp4?Expires=1672268640&Signature=DB6LZIBWScBsEtx-mZAXnMgqqqiD2PFWeW3NqeEFWQfpPKFzHiHUL5kpiFJj5IOBhDXIq3C2SHB4egt6~4NtWjbZYUXmAaA58IAPQGjKZ3lI9B1GiXJWEcUT5~sTfpsHgEUCCz2BoPXDWBzxbGpkSdbbwYyZfH6U~Stvu0vOY~wLuNKdTP36ckIK9~cyD0JsOKc-nR31j7qqfAZ4H2iXKJWCSzluuEKlWH~Ys7CLhkI4zIn8a8aLkWftQ8B4sb8EGN98n~flxTZARXt4yrsbxCxZU--yvAKgRjnBE~FB0uyRizpe3pRL4KMtV0xg1twuEK2KmXLUb2Obhr8LWMKQ2g__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
       imageSrc: reactMovie,
       url: "https://github.com/jwinbush/movie",
       demo: "https://intelo.vercel.app",
     },
     {
       id: 2,
-      video: "https://cdn-cf-east.streamable.com/video/mp4/euq4g0.mp4?Expires=1671959340&Signature=W6USbUzPeyafAM4XULK3-KfBICczVfIoTHFkfp9KhcsybMHI3qCStbSDIUJdQSxAMuVhVBCTDNDETOxyMGddBGXDxe9tFgqbdXUBfTTw6Rlm5Ku~ST1PB8iXe5ZrhA-g~QIsIl-D5A6W7Lalrm5aqaqD11se9yehBsoJY5Q9yP2VbuaFVpCUjUucNp~EndgEq6qg3q5K3DmnCA7RCsu-G4m~qafAKMcPtQshJkx-fL3yfIV1FzH5ktaU1gTlNJzJYfyXBXJDsv92fdZnO1TaKWOgsu-Dg4-g7iVdl5Xt4iaLebqlT5lhXHv6wnsdp5J9v3ve8gEfZlNsICpB9x~0Qw__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
+      video: "https://cdn-cf-east.streamable.com/video/mp4/euq4g0.mp4?Expires=1672268640&Signature=Y8JBxaVoIgZlDlvp~~BZNkVYVJYbaeWw8ypL~eFRu36lYZJWVM8cjjMzPr8ExBBt0oW6sWzJFFYj~QGNKX5eNNWNgTf5dKHTsthiVLOYFokMS8zjXcC3LUZdK0QLw-NS213PgymxyU3v5hTIyL51RfSMk-8qmfsIZNgWtWyZZMh-O6kmX20pwC5IYG1dlV6PHL7Xn1raaC8SzAfi1-eDJOsQKoiFmHh3psj5EfBixT0IHyoTtyui28rwKj4pScANM0be1lrYlSXqdVecTfsK3sigl4hRnupMcyt9VlUM1hVyHp~M558JFjikccUQox0My9qvZX75t1D-K193HypSBA__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
       imageSrc: nextSpotify,
       url: "https://github.com/jwinbush/spotify",
       demo: "https://www.jawonwinbush.com/login",
     },
     {
       id: 3,
-      video: "https://cdn-cf-east.streamable.com/video/mp4/jcljj3.mp4?Expires=1671960420&Signature=Modic2Z54FCEKEVdY5kFCHtSW3wap5rgDtHWlociaaF9-ks2b67DPRXCA5v9o-anP6-c1rxupC7H2NTscXpKPP~yl7gRYGGH9N~4HZa~y~Fz-2hgzH6bEE0OY4QHGaEt1UKxheG5zF80yrMY4BINTu2Nrn2Wbboo9eWH3Fg11vP40uW7n5ZWGMwb6qPqYHd1s7lW3-sn5wR8cWTS8UoCci-YU6lRZu1-55aRVhS2Oh56Ch4IAcRPNUt~m9VSWupGuRztI-XAVR4w9WdfmFHEEqPgXvCWrK1GaxlxD8SvswLRPe0ItNbhtLuWs9RdEVAy7ConSrkm4jyOdTsQftzJRg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
+      video: "https://cdn-cf-east.streamable.com/video/mp4/jcljj3.mp4?Expires=1672268640&Signature=Yx74v62KA~h34AKBGHQW65YgfAn5aGIyApDjBsoMaIrzxoYAf9EMASjefhZ4xsPzhOoH6EqTAU2ixWweoaLifDCn0VLh65zO8FPpLrgehAa8fGNARkAGgj9GSIGKD-OFxrXacYPyE6P9WPp~f~bfLS1vwBkeT7xh2o2sIrXJ6rVpxHuMh~wjsDv0HFRrSqoRMmZb9wA1BneiyjIT0XZPkfvJuKLL30jSy16SwjjPJPfC6XQMLovdcraJSbUecw0bm-Q75H5CDLFAnVdjXIq~c72ezdTSnrCAlDrPU1x9su8b541q5xzVZBXiR1XZml3iziM6m9r4ZfR69-fHKgvBlw__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
       imageSrc: phpRental,
       url: "https://github.com/jwinbush/OdysseyRentals",
       demo: "",
