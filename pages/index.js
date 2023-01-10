291f7aaea90717949fccb52729fa349673d30986
J.M Winbush <jawonwinbush@gmail.com>
2023-01-09 21:47:51 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,11 @@ import Portfolio from "../pages/portfolio";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { BsFillMoonFill, BsList } from "react-icons/bs";
 import { useState } from "react";
-import jawon from "../public/AIpicture.png";
+import jawon from "../public/PortraitofMe2.jpg";
 import backend from "../public/backend.png";
 import frontend from "../public/frontend.png";
 import more from "../public/more.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import { motion as m } from "framer-motion";
 
 export default function Home() {
